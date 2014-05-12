dbccc5bc8ec3ffa0287baada2cab9967137bbdab
Anjaly Saju <anjalysaju123@gmail.com>
2014-05-12 17:53:31 +0530

    auth & index file updated

--- Auth.php
+++ Auth.php
@@ -1,39 +1,86 @@
+<html>
 <?php
-
-$db_username='root';
-$db_password='anjalyes';
-$db_database='Admin_Master';
-$db_hostname='localhost';
-
-$db_server = mysql_connect($db_hostname,$db_username,$db_password);
-
-if(!$db_server)
-	die("Unable To Connect To Mysql: ".mysql_error());
-
-mysql_select_db($db_database)or die("Unable to access db: ".mysql_error());
-
-$query="SELECT * FROM Admin_users WHERE Uname=$_COOKIE['user'] AND PassHash=$_COOKIE['pass']";
-$result=mysql_query($query);
-
-if (!$result) {
-	die("Unable to access db: ".mysql_error());
-}
-elseif (mysql_num_rows($result)) {
-	$row=mysql_fetch_row($result);
-	if ($pass==$row[3]) {
-		session_start();
-		$_SESSION['user']=$user;
-		$_SESSION['pass']=$pass;
-		$id=uniqid();										//CREATING AUTH ID
-		$authid=md5($id)
-		$_SESSION['authkey']=$authid;
-		$_COOKIE['authkey']=$_SESSION['authkey'];
-		echo "Welcome ".$row[2]."! Please <a href=Adminhome.php>Click Here</a> to continue";
-	}
-	else
-		die("invalid username/password");
-}
-else
-	die("invalid username/password");
-
+ini_set('display_errors', '1');
+include 'config.php';
 ?>
+
+<title><?php echo "Redirecting..."; ?></title>
+<body>
+
+	<?php
+
+		$db_server = mysql_connect($db_hostname,$db_username,$db_password);
+
+		if(!$db_server)
+			die("Unable To Connect To Mysql: ".mysql_error());
+
+		mysql_select_db($db_database) or die("Unable To Connect To Mysql: ".mysql_error());
+
+		if (check()) {
+			
+			$user=fixstring($_POST['user']);
+			$pass=fixstring($_POST['pass']);
+
+			$query="SELECT * FROM Admin_users WHERE Uname='$user'";
+			$result=mysql_query($query);
+
+			if(!$result)
+				die("Unable to access db: ".mysql_error());
+
+			elseif(mysql_num_rows($result)){
+
+				$row=mysql_fetch_row($result);
+
+				if ($pass==$row[3]){
+				
+					session_start();							
+					$_SESSION['user']=$user;
+					$_SESSION['pass']=$pass;
+					$id=uniqid();						//creating new id;
+					$id=md5($id);
+					$_SESSION['authkey']=$id;
+					$_COOKIE['authkey']=$id;
+					header('location: Adminhome.php');
+				}
+				else 
+					die("Invalid username/password");
+			}
+			else
+				die("User doesn't exist!");			
+		}
+		else{
+
+			echo "Username & Password Required<br><br>";
+			echo "Please <a href=index.php> click here </a> to log in again.";
+		}
+
+		//check if valid values are passed
+		function check(){
+
+			if(isset($_POST)){
+				if (!isset($_POST['user'])||$_POST['user']=='') {
+					return false;
+				}
+				if (!isset($_POST['pass'])||$_POST['pass']=='') {
+					return false;
+				}
+				if (isset($_POST['user'])&& $_POST['user']!='' && isset($_POST['pass'])&& $_POST['pass']!='') {
+					return true;			
+				}
+
+			}
+		}
+
+		function fixstring($string){
+			return htmlentities(mysql_fix_entities($string));
+		}
+
+		function mysql_fix_entities($string){
+			if (get_magic_quotes_gpc()) {
+				$string=stripslashes($string);
+			}
+			return mysql_real_escape_string($string);
+		}
+	?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,69 +1,39 @@
-<?php
-$db_username='root';
-$db_password='anjalyes';
-$db_database='Admin_Master';
-$db_hostname='localhost';
-
-$db_server = mysql_connect($db_hostname,$db_username,$db_password);
-
-session_start();
-
-if(!$db_server)
-	die("Unable To Connect To Mysql: ".mysql_error());
-
-mysql_select_db($db_database)or die("Unable to access db: ".mysql_error());
-
-/*$query="INSERT INTO Admin_users(ID,Uname,Name,PassHash) VALUES('12','Anjaly','AES','SHASH')";
-$result=mysql_query($query);
-
-if(!$result) 
-	die("ERROR: ".mysql_error());
-else
-	echo "done!";*/
-function check_login(){								//check if logged in
-	if(isset($_SESSION['authkey'])){									//check if a session is active:if yes,check if cookie id=session id. Else login 
-
-		if($_SESSION['authkey']==$_COOKIE['authkey'])									
-			return true;
-	else
-			return false;
-}
-}
-
-if (check_login()) 
-	header('Location: Adminhome.php');
-
-else{
-	if(isset($_POST['user'])&&isset($_POST['pass'])){
-
-		$user=fixstring($_POST['user']);
-		$pass=fixstring($_POST['pass']);
-		$_COOKIE['user']=$user;
-		$_COOKIE['pass']=$pass;
-	}
-
-function fixstring($string){
-	return htmlentities(mysql_fix_string($string));
-}
-
-function mysql_fix_string($string){
-	if (get_magic_quotes_gpc()) {
-		$string=stripslashes($string);
-	}
-	return mysql_real_escape_string($string);
-}
-
-echo<<<_END
 <html>
-<head>Login Form</head>
-<body><pre>Enter user name and password
-
-<form method='post' action='Auth.php'>
-	Username <input type="text" name="user" size="7">
-	Password <input type="text" name="pass" size="7">
-			 <input type="submit" value="Log In!"></form>
-
-</pre></body>
-</html>
-_END;
+<?php
+ini_set('display_errors', '1');
 ?>
+
+<title><?php echo "Login Form"; ?></title>
+<body>
+
+<?php
+
+	session_start();
+
+	function check_login(){						//to check if logged in;
+	
+		if (isset($_SESSION['authkey'])){
+
+			if ($_SESSION['authkey']==$_COOKIE['authkey']) // Notice: Undefined index: authkey in /var/www/index.php (i'm getting this error..)
+				return true;
+			else
+				return false;
+		}
+		else
+			return false;
+	}
+
+	if (check_login()) {							//if logged in, redirect to home pg;
+		header('location: Adminhome.php');
+	}
+?>
+
+<pre>Enter username and password: 
+	<form method='post' action='Auth.php'>
+		Username <input type="text" name="user" size="7">
+		Password <input type="text" name="pass" size="7">
+				 <input type="submit" value="Log In!"> 
+	</form>
+</pre>
+</body>
+</html>
